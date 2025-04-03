31a59e59af4d4e1e950a45ee4b98112fa41a1782
Stephen-matheka <smatheka080@gmail.com>
2025-04-03 15:45:43 +0300

    test.py

--- test.py
+++ test.py
@@ -1,14 +1,14 @@
 def total(a, b):
-  return a+b
+    return a + b
 
-def sub(a,b):
-  return a-b
+def sub(a, b):
+    return a - b
 
-def mult(a,b):
-  return a*b
+def mult(a, b):
+    return a * b
 
-def division(a,b):
-  return a/b
+def division(a, b):
+    return a / b
 
 print('''
 1. Add
@@ -19,20 +19,28 @@ print('''
 
 choice = int(input("CHOOSE OPTIONS: "))
 
-switch(choice):
-  case 1:
-     num1 = int(input("Enter the first number: "))
-     num2 = int(input("Enter the second number: "))
-     print(total(num1, num2))
-  case 2:
-     num1 = int(input("Enter the first number: "))
-     num2 = int(input("Enter the second number: "))
-     print(sub(num1, num2))
-  case 3:
-       num1 = int(input("Enter the first number: "))
-       num2 = int(input("Enter the second number: "))
-       print(mult(num1, num2))
-  case 4:
-       num1 = int(input("Enter the first number: "))
-       num2 = int(input("Enter the second number: "))
-       print(division(num1, num2))
+if choice == 1:
+    num1 = int(input("Enter the first number: "))
+    num2 = int(input("Enter the second number: "))
+    print(total(num1, num2))
+
+elif choice == 2:
+    num1 = int(input("Enter the first number: "))
+    num2 = int(input("Enter the second number: "))
+    print(sub(num1, num2))
+
+elif choice == 3:
+    num1 = int(input("Enter the first number: "))
+    num2 = int(input("Enter the second number: "))
+    print(mult(num1, num2))
+
+elif choice == 4:
+    num1 = int(input("Enter the first number: "))
+    num2 = int(input("Enter the second number: "))
+    if num2 == 0:
+        print("Error! Division by zero.")
+    else:
+        print(division(num1, num2))
+
+else:
+    print("Invalid choice! Please select a valid option.")
